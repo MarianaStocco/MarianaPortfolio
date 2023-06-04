ed0df319f878d16f113972aaa5afe75257d263ba
Mariana Stocco <mariana.stocco@outlook.com>
2023-06-04 18:48:44 -0300

    04-06- Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { styles } from '../style';
-// import { Computer } from './canvas';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
@@ -9,12 +9,16 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#00ff00]'/>
-          <div className='w-1 sm:h-80 h-40 green-gradient'/>
+          <div className='w-1 sm:h-80 h-40 bg-[#00ff00] bg-gradient-to-b'/>
         </div>
           <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#00ff00]'>Mariana</span></h1>
+            <h1 className={`${styles.heroHeadText} text-gray`}>Hi, I'm <span className='text-[#00ff00]'>Mariana</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-gray-100`}>
+              I develop great web applications <br className='sm:block hidden'/> and user interfaces
+            </p>
           </div>
       </div>
+      <ComputersCanvas/>
     </section>
   )
 }
