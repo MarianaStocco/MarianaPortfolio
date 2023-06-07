c27a24e65e577a737c3483c1c7aecde32774b37d
Mariana Stocco <mariana.stocco@outlook.com>
2023-06-07 16:53:45 -0300

    07-06 Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
         <About />
         {/* <Experience /> */}
         <Tech />
-        <Works />
         <Feedbacks />
+        <Works />
         <div className='relative z-0'>
           <Contact />
           {/* <StarsCanvas /> */}
